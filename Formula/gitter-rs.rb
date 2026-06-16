acf38aa959b8370497293cf03a7c7c1e8931397a
indrajit <eendroroy@gmail.com>
2026-06-16 11:26:16 +0600

    update readme

--- Formula/gitter-rs.rb
+++ Formula/gitter-rs.rb
@@ -4,24 +4,18 @@ class GitterRs < Formula
   version "0.1.0"
   license "AGPL-3"
 
-  # --- MAC APPLE SILICON (aarch64) ---
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/eendroroy/gitter-rs/releases/download/#{version}/gitter-aarch64-apple-darwin.zip"
     sha256 "1d13afc86891c19b490932c1732c3e071a2e6294a1684518f6153488792499f1"
-
-  # --- MAC INTEL (x86_64) ---
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/eendroroy/gitter-rs/releases/download/#{version}/gitter-x86_64-apple-darwin.zip"
     sha256 "5a2a957c68958a1ef4714febcd2030062a516418c555a9a52ba2776818cbf3dd"
-
-  # --- LINUX AMD64 / x86_64 ---
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/eendroroy/gitter-rs/releases/download/#{version}/gitter-x86_64-unknown-linux-gnu.zip"
     sha256 "a17e3cc6f3e8a3ed0799bed8ff1801eb78e2b687d84137e4dcbcf7fefdcf8c40"
   end
 
   def install
-    # Binary name inside the zip archive is likely "gitter"
     bin.install "gitter"
   end
 
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-tools
+
+This repository contains various tools for Homebrew.
+
+## Usage
+
+Tap the repository: `brew tap eendroroy/tools`
+Install formula/cask `brew install [--cask] eendroroy/tools/<formula>`
+
+## Available Formulas
+
+*   **[gitter-rs](https://github.com/eendroroy/gitter-rs)**: `brew install eendroroy/tools/gitter-rs`
+
+## License
+
+This project is licensed under the MIT License - see the `LICENSE` file for details.
